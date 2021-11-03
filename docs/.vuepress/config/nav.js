059bade478347ddf02486720cd442b09ccb079b7
dhy <1228909324@qq.com>
2021-11-04 00:01:22 +0800

    first commit

---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,33 @@
+// nav
+module.exports = [
+  { text: '首页', link: '/' },
+  {
+    text: '前端',
+    link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+  },
+  {
+    text: '算法',
+    link: '/algorithm/',
+  },
+  {
+    text: '技术',
+    link: '/technology/',
+  },
+  {
+    text: '更多',
+    link: '/more/',
+    items: [
+      { text: '友情链接', link: '/friends/' },
+    ]
+  },
+  { text: '关于', link: '/about/' },
+  {
+    text: '索引',
+    link: '/archives/',
+    items: [
+      { text: '分类', link: '/categories/' },
+      { text: '标签', link: '/tags/' },
+      { text: '归档', link: '/archives/' },
+    ],
+  },
+]
---
+++ docs/index.md
@@ -0,0 +1,23 @@
+---
+home: true
+heroText: Haoyuan's blog
+tagline: Web个人学习博客，学习是一件快乐的事情。
+
+features: # 可选的
+  - title: Web
+    details: Web全栈框架、语言等技术
+    link: /web/ # 可选
+    imgUrl: /img/web.png # 可选
+  - title: 算法
+    details: 数据结构与算法
+    link: /algorithm/
+    imgUrl: /img/machine-learning.png
+  - title: 技术
+    details: 技术文档、教程、技巧、总结等文章
+    link: /technology/
+    imgUrl: /img/bulb.png
+
+# 文章列表显示方式: detailed 默认，显示详细版文章列表（包括作者、分类、标签、摘要、分页等）| simple => 显示简约版文章列表（仅标题和日期）| none 不显示文章列表
+# postList: detailed
+# simplePostListLength: 10 # 简约版文章列表显示的文章数量，默认10。（仅在postList设置为simple时生效）
+---
