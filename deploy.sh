39926a816b518972b0024cfa8a2b5e0bfd8045e7
dhy <haoyuan1996@qq.com>
2021-11-05 00:27:52 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,12 +14,12 @@ echo 'denghaoyuan.com' > CNAME
 
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
-  githubUrl=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+  githubUrl=git@github.com:Somnus711/Somnus711.github.io.git
 else
   msg='来自github actions的自动部署'
-  githubUrl=https://xugaoyi:${GITHUB_TOKEN}@github.com/xugaoyi/vuepress-theme-vdoing.git
-  git config --global user.name "xugaoyi"
-  git config --global user.email "894072666@qq.com"
+  githubUrl=https://dhy:${ACCESS_TOKEN}@github.com:Somnus711/Somnus711.github.io.git
+  git config --global user.name "dhy"
+  git config --global user.email "haoyuan1996@qq.com"
 fi
 git init
 git add -A
